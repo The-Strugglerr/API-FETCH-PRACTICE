947110e8644c3b0afc619433055d7ee88ff4ce71
Naman Yadav <raymannky2001@gmail.com>
2023-07-04 19:20:40 +0530

    updating a version two 2nd country will appear only after first call back hell implementation

--- script.js
+++ script.js
@@ -5,51 +5,56 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryData = function (con) {
+const addContry = function (data, className = '') {
+  const propertyValuesLanguages = Object.values(data.languages);
+  const propertyValuesCurrencies = Object.values(data.currencies);
+
+  const lang =
+    propertyValuesLanguages.length === 1
+      ? propertyValuesLanguages[0]
+      : propertyValuesLanguages[1];
+
+  const html = `<article class="country ${className}  ">
+  <img class="country__img" src="${data.flags.svg}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} Million people</p>
+    <p class="country__row"><span>🗣️</span>${lang}</p>
+    <p class="country__row"><span>💰</span>${
+      propertyValuesCurrencies[0].name
+    }</p>
+  </div>
+</article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (con) {
   const request = new XMLHttpRequest();
-
-  // now we need a url to which we do ajax call , below first we specify like did i need information of i am sendind info with the link to which we are talking
-
   request.open('GET', `https://restcountries.com/v3.1/name/${con}`);
-
-  // above we specift like what we want with requests
-
-  // now we need to send it
-
   request.send();
-
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
-    const propertyValuesLanguages = Object.values(data.languages);
-    const propertyValuesCurrencies = Object.values(data.currencies);
+    //first country
+    addContry(data);
 
-    const lang =
-      propertyValuesLanguages.length === 1
-        ? propertyValuesLanguages[0]
-        : propertyValuesLanguages[1];
+    // neighbouring country
+    const [neighbour] = data.borders;
 
-    const htmll = `<article class="country">
-    <img class="country__img" src="${data.flags.svg}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name.common}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)} Million people</p>
-      <p class="country__row"><span>🗣️</span>${lang}</p>
-      <p class="country__row"><span>💰</span>${
-        propertyValuesCurrencies[0].name
-      }</p>
-    </div>
-  </article>`;
-
-    countriesContainer.insertAdjacentHTML('beforeend', htmll);
-    countriesContainer.style.opacity = 1;
+    if (!neighbour) return;
+    const request1 = new XMLHttpRequest();
+    request1.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request1.send();
+    request1.addEventListener('load', function () {
+      const [data1] = JSON.parse(this.responseText);
+      console.log(data1);
+      addContry(data1, 'neighbour');
+    });
   });
 };
 
-getCountryData('india');
-getCountryData('china');
-getCountryData('canada');
-getCountryData('japan');
+getCountryAndNeighbour('india');
