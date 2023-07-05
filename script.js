3e1b243879df74df6f954fa41a17a07fb347df1e
Naman Yadav <raymannky2001@gmail.com>
2023-07-05 08:43:56 +0530

    adding promises chain to avoid call back hell

--- script.js
+++ script.js
@@ -33,25 +33,28 @@ const renderCountry = function (data, className = '') {
 };
 
 /*
-using fetch methods it will immediately return a promise and in beginning this promsise will be stilll pending and at some point this promise will get settle , it it will be fulfilled then it will return something and that response we handle with then method, it is only take one parameter that is the response from fetch method.in this response we have a body property in which we have all the data so to acces it we need to use another method called json but above is problem this json is also asynchronous function so it will also return a promise so we need to return it and then call again a then method.
+Implementation of neighbors how to overcome the callback hell here see
+rememeber one thing what a then method return is always a fullfill value.
+i will add ss to it below.
+
 */
-
-// const addContry = function (con) {
-//   fetch(`https://restcountries.com/v3.1/name/${con}`)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//     });
-// };
-
-// lets use arrow function to simplfy above
 
 const addContry = function (con) {
   fetch(`https://restcountries.com/v3.1/name/${con}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      console.log(data[0]);
+      renderCountry(data[0]);
+      const noOfNeighbors = data[0].borders.length;
+      if (!noOfNeighbors) return;
+      const random = Math.floor(Math.random() * noOfNeighbors);
+
+      const neighbor = data[0].borders[random];
+      console.log(neighbor);
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbor}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'));
 };
 
-addContry('india');
+addContry('pakistan');
